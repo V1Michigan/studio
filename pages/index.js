462744465a655606b034fdc4d92eb0931d83ffdf
samay <samay@umich.edu>
2021-05-07 15:05:24 -0400

    header

--- pages/index.js
+++ pages/index.js
@@ -22,25 +22,20 @@ export default function IndexPage() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <div className="h-3/4 flex justify-center items-center bg-blue-100 p-8">
+      <div className="h-3/4 flex justify-center items-center bg-gray-100 p-12">
         <div className="max-w-screen-xl">
-          <div className="md:flex">
-            <div className="max-w-xl p-4">
-            
-              {/* <img className="tilt shadow-md rounded-sm" src="/gif.gif"></img> */}
+          <div className="md:flex md:flex-col md:items-center">
+            <div className="max-w-xl">
+              <img className="w-24 opacity-50 animate-spin-slow" src="/atom.svg"/>
             </div>
-            <div className="flex-1 p-8 flex items-center">
+            <div className="flex-1 flex my-2 items-center">
               <div className="">
-                <h1 className="text-6xl tracking-tight font-bold font-logo text-gray-900 leading-none" >
+                <h1 className="text-6xl tracking-tighter font-bold font-logo text-blue-600 leading-none">
                   V1 Product Studio
                 </h1>
-                <h2 className="text-2xl tracking-tight font-bold italic text-gradient bg-gradient-to-r from-yellow-600 to-yellow-700">
+                <h2 className="text-2xl tracking-tight italic text-gradient bg-gradient-to-r from-yellow-600 to-yellow-700">
                   at Michigan
                 </h2>
-                <p className="text-2xl mt-8 text-gray-900 font-bold tracking-tight mb-3">
-                  The community for ambitious student builders at the University
-                  of Michigan.
-                </p>
               </div>
             </div>
           </div>
