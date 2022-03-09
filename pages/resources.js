01bc5d270a271ced7402aa44617a3306ebf5852c
Elliot Klein <elliotfklein@gmail.com>
2022-03-09 10:33:19 -0500

    feat: update /resources redirect with Super URL

--- pages/resources.js
+++ pages/resources.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function ResourcesRedirect() {
   useEffect(() => {
-    window.location.replace('https://exciting-banon-082.notion.site/Product-Studio-Resources-Homepage-c1a551ca3a204b268dab50c87c8e0cac');
+    window.location.replace('https://product-studio-resources.super.site/');
   }, []);
   return null;
 }
